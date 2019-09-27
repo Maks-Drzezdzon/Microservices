053bfbd48bb36e825f2a5919c7bbf08144984418
Maksthorn <Maksdrzezdzon@gmail.com>
2019-09-27 17:51:02 +0100

    adding histrix

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,12 @@ package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EnableCircuitBreaker
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
